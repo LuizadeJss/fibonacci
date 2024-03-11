020dd25593934900113694cc7b8c1ecd9ea681dc
LuizadeJss <ljff_luz@hotmail.com>
2024-03-11 12:07:22 -0300

    verificando se um número digitado pertence a sequencia de fibonacci ou não

---
+++ fibonacci/src/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static int fibonacci(int n){
+        if(n<=1)
+            return n;
+        else
+            return fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int numero = entrada.nextInt();
+        boolean pertenceOuNao = false;
+
+        for (int i = 1; i < numero; i++) {
+            if(fibonacci(i) == numero){
+                pertenceOuNao = true;
+            }
+        }
+
+        if(pertenceOuNao)
+            System.out.println("Pertence a sequência de Fibonacci");
+        else
+            System.out.println("Não pertence a sequência de Fibonacci");
+    }
+}
